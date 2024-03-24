6c32047f68d59da9e8e84db5147f3ff380b65592
Sagar <sagarsalgar280@gmail.com>
2024-03-24 14:41:23 +0530

    almost done

--- JS/style-switcher.js
+++ JS/style-switcher.js
@@ -25,11 +25,11 @@ const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
+    document.body.classList.toggle("bright");
 })
 
 window.addEventListener("load", () => {
-    if(document.body.classList.contains("dark")){
+    if(document.body.classList.contains("bright")){
         dayNight.querySelector("i").classList.add("fa-sun");
     }
     else{
